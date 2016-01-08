8b11e65778c5a4d6f99daaa07b439bb66568530b
soedomoto <soedomoto@gmail.com>
2016-01-08 14:14:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,3 +48,91 @@ with open("/opt/cassandra/conf/cassandra.yaml", "w") as outfile:
     outfile.write(yaml.dump(cassyaml))
 EOF
 
+# build cassandra-lucene-index
+git clone -b branch-2.2.4 --single-branch https://github.com/Stratio/cassandra-lucene-index.git
+cd cassandra-lucene-index
+mvn clean package -Ppatch -Dcassandra_home=/opt/cassandra
+
+chown -R cassandra /opt/cassandra
+
+# export PATH=$PATH:/opt/cassandra/bin
+# source ~/.profile
+
+echo '#!/bin/sh
+# chkconfig: 2345 80 45 
+# description: Starts and stops Cassandra 
+# update deamon path to point to the cassandra executable 
+
+CASS_HOME=/opt/cassandra
+CASS_BIN="$CASS_HOME/bin/cassandra"
+CASS_PID="$CASS_HOME/cassandra.pid"
+
+start() { 
+    if [ -f $CASS_PID ]; then 
+        echo "Cassandra is already running." 
+        exit 0 
+    fi 
+
+    echo -n "Starting Cassandra... "
+    
+    start-stop-daemon -S -c cassandra -a "$CASS_BIN" -q -p "$CASS_PID" -t
+    start-stop-daemon -S -c cassandra -a "$CASS_BIN" -b -p "$CASS_PID" -- -p "$CASS_PID"
+    
+    echo "OK"
+    return 0
+}
+
+stop() { 
+    if [ ! -f $CASS_PID ]; then
+        echo "Cassandra is already stopped."
+        exit 0
+    fi
+
+    echo -n "Stopping Cassandra... " 
+
+    #kill $(cat $CASS_PID) 
+    start-stop-daemon -K -p "$CASS_PID" -R TERM/30/KILL/5 >/dev/null
+    RET=$?
+    rm -f "$CASS_PID"
+    return $RET
+}
+
+status_fn() { 
+    if [ -f $CASS_PID ]; then 
+        echo "Cassandra is running." 
+        exit 0 
+    else 
+        echo "Cassandra is stopped." 
+        exit 1 
+    fi
+}
+
+case "$1" in 
+    start) 
+        start 
+        ;; 
+    stop) 
+        stop 
+        ;; 
+    status) 
+        status_fn 
+        ;; 
+    restart) 
+        stop 
+        start 
+        ;; 
+    *) 
+        echo $"Usage: $prog {start|stop|restart|status}" 
+        exit 1 
+esac 
+
+exit $?
+' | tee /etc/init.d/cassandra
+
+chmod +x /etc/init.d/cassandra
+update-rc.d cassandra defaults
+
+echo -e "\nCassandra has been installed succesfully..."
+echo -e "Start service with command : "
+echo -e "    sudo service cassandra start\n"
+
